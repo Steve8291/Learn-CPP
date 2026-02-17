f69db6f060641baee22f98379dec2b90c25ab2c8
Steve8291 <hambling8@gmail.com>
2026-02-17 11:31:50 -0500

    Add assignment instructions for calculating dog years in comments

--- Lessons/variables.cpp
+++ Lessons/variables.cpp
@@ -80,3 +80,20 @@ void loop() {
         printSummary();
     }
 }
+
+/*
+Assignment: Write a program to calculate how old a dog is in "dog years".
+
+Dogs mature at a faster rate than human beings. 
+If you own a dog, you might’ve heard this rule: one year for Snoopy equals seven years for you. 
+Turns out, the math isn’t that simple. Dogs mature more quickly than we do early on.
+
+Here’s how we can convert your dog’s age into human years:
+
+   * The first two years of a dog’s life count as 21 human years.
+   * Each following year counts as 4 human years.
+
+So a one-year-old dog is 10.5 years old in human years, and a two-year-old dog is 21 years old in human years… 
+
+Write a C++ program called dog_years.cpp to calculate the age, in human years, of any dog older than 2.
+*/
