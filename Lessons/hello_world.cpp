9654858177a3c6634c19983c7b6594231e90f97c
Steve8291 <hambling8@gmail.com>
2026-02-17 11:26:23 -0500

    Implement ESP32 variable demonstration with user input handling and data summary

---
+++ Lessons/hello_world.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+void setup() {
+  // Initialize the serial communication at 115200 baud rate
+  Serial.begin(115200); 
+}
+
+void loop() {
+  // Print "Hello World!" to the Serial Monitor
+  Serial.println("Hello World!");
+  Serial.print("Welcome to ");
+  Serial.println("My WORLD!!");
+  Serial.println("");
+  Serial.println("C++ programming on ESP32!");
+  Serial.print("Harmony School\nAwesome!\n\n");
+
+
+  // Wait for 2 seconds before repeating the loop
+  delay(2000); 
+}
+
+// Assignment: Create your name in block letters using Serial.print() and Serial.println() functions.
+// You can use spaces and new lines to format your name as you like. For example:
