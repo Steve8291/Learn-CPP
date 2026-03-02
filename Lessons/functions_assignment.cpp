81dfbfd70e0a4249afb856f2e8776f3cd5c7af6e
Steve8291 <hambling8@gmail.com>
2026-03-02 11:27:52 -0500

    functions

---
+++ Lessons/functions_assignment.cpp
@@ -0,0 +1,43 @@
+/* Assignment: Traffic Light
+Build a traffic light controller using functions. 
+The traffic light should cycle through green, yellow, and red lights with appropriate delays. 
+Use functions to represent each light state and the transitions between them. For example:
+    void greenLight() {
+        // Code to turn on green light
+        delay(5000); // Green light for 5 seconds
+    }
+*/
+
+# include <Arduino.h>
+
+// Define Pins
+const int RED = 32;
+const int YELLOW = 33;
+const int GREEN = 25;
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+}
+
+// A function that activates the light with a 2 parameters: 'light_pin' and 'duration'
+// This replaces repeating digitalWrite(pin, HIGH) everywhere
+// It should have a delay for the duration of the light, then turn it off
+// This function should be able to work for any light color and duration, so it needs parameters
+
+
+// A caution flash function that flashes the yellow light a certain number of times, with a delay in between
+// This function should call your previous function (Abstraction)
+// It should take in a parameter for how many times to flash
+// Use a for loop to repeat the flashing the correct number of times
+//  for(int i = 0; i < flashes; i++)
+
+
+
+void loop() {
+// In your loop, call the function to activate each light in the correct order with the correct durations.
+// Then call the caution flash function before the loop repeats to add a blinking yellow light between cycles.
+
+
+}
